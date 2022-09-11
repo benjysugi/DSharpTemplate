0bc5a7046fad3e05a4d62705c3bf763d05dd9477
ベンジー <benjysugi01@outlook.com>
2022-09-11 17:59:59 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Documentation:
+// https://dsharpplus.github.io/articles/basics/bot_account.html
+// https://dsharpplus.github.io/articles/basics/first_bot.html
+
+namespace ConsoleApp1 {
+    internal class Program {
+        static void Main(string[] args) {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync() {
+
+        }
+    }
+}
